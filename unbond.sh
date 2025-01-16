e0ec7eb2bf53bceafdc62b62a6fc51461fb0e516
Motko <77466238+Motko222@users.noreply.github.com>
2025-01-16 10:42:27 +0100

    Update unbond.sh

--- unbond.sh
+++ unbond.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#usage: bash delegate.sh <key> <valoper> <amount>
+#usage: bash unbond.sh <key> <from_valoper> <amount>
 
 folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
 source ~/scripts/$folder/cfg
